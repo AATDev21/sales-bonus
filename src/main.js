2aeef9f87f826da61510f8fa4f49f20964a121bf
Alina <tikhovaaa@gmail.com>
2025-09-13 01:46:56 +0300

    Sprint 5. Attempt 3.

--- src/main.js
+++ src/main.js
@@ -18,13 +18,13 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     if (index === 0) {
-        return seller.totalProfit * 0.15;
+        return seller.profit * 0.15;
     } else if (index === 1 || index === 2) {
-        return seller.totalProfit * 0.1;
+        return seller.profit * 0.1;
     } else if (index === total - 1) {
         return 0;
     } else { // Для всех остальных
-        return seller.totalProfit * 0.05;
+        return seller.profit * 0.05;
     }
 }
 
@@ -69,12 +69,12 @@ function analyzeSalesData(data, options) {
   }
 
   const sellersStats = data.sellers.map((seller) => ({
-    sellerId: seller.id,
-    sellerName: `${seller.first_name} ${seller.last_name}`,
-    totalRevenue: 0,
-    totalProfit: 0,
-    salesCount: 0,
-    products: new Map(),
+    id: seller.id,
+    name: `${seller.first_name} ${seller.last_name}`,
+    revenue: 0,
+    profit: 0,
+    sales_count: 0,
+    products_sold: new Map(),
   }));
 
   const sellersIndex = data.sellers.reduce((acc, seller) => {
@@ -88,7 +88,7 @@ function analyzeSalesData(data, options) {
   }, {});
 
   const sellersStatsIndex = sellersStats.reduce((acc, stat) => {
-    acc[stat.sellerId] = stat;
+    acc[stat.id] = stat;
     return acc;
   }, {});
 
@@ -96,7 +96,7 @@ function analyzeSalesData(data, options) {
     const sellerStat = sellersStatsIndex[record.seller_id];
     if (!sellerStat) return;
 
-    sellerStat.salesCount++;
+    sellerStat.sales_count++;
 
     record.items.forEach((item) => {
       const product = productsIndex[item.sku];
@@ -107,27 +107,27 @@ function analyzeSalesData(data, options) {
         (item.sale_price * (1 - item.discount / 100) - product.purchase_price) *
         item.quantity;
 
-      sellerStat.totalRevenue += revenue;
-      sellerStat.totalProfit += profit;
+      sellerStat.revenue += revenue;
+      sellerStat.profit += profit;
 
-      if (!sellerStat.products.has(product.sku)) {
-        sellerStat.products.set(product.sku, {
+      if (!sellerStat.products_sold.has(product.sku)) {
+        sellerStat.products_sold.set(product.sku, {
           product,
-          totalRevenue: 0,
-          totalProfit: 0,
+          revenue: 0,
+          profit: 0,
           quantity: 0,
         });
       }
 
-      const productStat = sellerStat.products.get(product.sku);
-      productStat.totalRevenue += revenue;
-      productStat.totalProfit += profit;
+      const productStat = sellerStat.products_sold.get(product.sku);
+      productStat.revenue += revenue;
+      productStat.profit += profit;
       productStat.quantity += item.quantity;
     });
   });
 
   const sortedSellers = [...sellersStats].sort(
-    (a, b) => b.totalProfit - a.totalProfit
+    (a, b) => b.profit - a.profit
   );
 
   const totalSellers = sortedSellers.length;
@@ -136,22 +136,22 @@ function analyzeSalesData(data, options) {
   });
 
   const result = sortedSellers.map((sellerStat) => {
-    const topProducts = Array.from(sellerStat.products.values())
+    const topProducts = Array.from(sellerStat.products_sold.values())
       .sort((a, b) => b.quantity - a.quantity)
       .slice(0, 10);
 
     return {
-      seller_id: sellerStat.sellerId,
-      name: sellerStat.sellerName,
-      revenue: Number(sellerStat.totalRevenue.toFixed(2)),
-      profit: Number(sellerStat.totalProfit.toFixed(2)),
-      sales_count: sellerStat.salesCount,
-      bonus: Number(sellerStat.bonus.toFixed(2)),
+      seller_id: sellerStat.id,
+      name: sellerStat.name,
+      revenue: +sellerStat.revenue.toFixed(2),
+      profit: +sellerStat.profit.toFixed(2),
+      sales_count: sellerStat.sales_count,
+      bonus: +sellerStat.bonus.toFixed(2),
       top_products: topProducts.map((product) => ({
         sku: product.product.sku,
         name: product.product.name,
-        revenue: Number(product.totalRevenue.toFixed(2)),
-        profit: Number(product.totalProfit.toFixed(2)),
+        revenue: +product.revenue.toFixed(2),
+        profit: +product.profit.toFixed(2),
         quantity: product.quantity,
       })),
     };
